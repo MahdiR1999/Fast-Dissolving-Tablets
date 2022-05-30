858db71fba20c797fea3901aaa802118d62cbbb8
MahdiR1999 <106333571+MahdiR1999@users.noreply.github.com>
2022-05-30 20:21:17 +0430

    Update References.js

--- src/Pages/AI/References.js
+++ src/Pages/AI/References.js
@@ -2,7 +2,7 @@ import {Card} from "primereact/card";
 import {useState} from "react";
 
 function References() {
-    const [Concs] = useState(`Hannan PA, Khan JA, Khan A, Safiullah S. Oral dispersible system: a new approach in drug delivery syste. Indian J Pharm Sci 2016;78:2-7.
+    const [Concs] = useState(` Hannan PA, Khan JA, Khan A, Safiullah S. Oral dispersible system: a new approach in drug delivery syste. Indian J Pharm Sci 2016;78:2-7.
     Bhowmik D, Chiranjib B, Krishnakanth, Pankaj, Chandira RM. Fast dissolving tablet: an overview. J Chem Pharm Res 2009;1:163-77.
     Siddiqui N, Garg G, Sharma PK. Fast dissolving tablets: preparation, characterization and evaluation: an overview. Int J Pharm Sci Rev Res 2010;2:87-96.
     Gupta DK, Bajpai M, Chatterjee DP. Fast mouth is dissolving disintegrating tablet and patient counselling points for FDDTSa review. Int J Res Dev Pharm L Sci 2014;3:949-58.
@@ -12,7 +12,7 @@ function References() {
     Ashish P, Harsoliya MS, Pathan JK, Shruti S. A review: formulation of mouth dissolving tablet. Int J Pharm Res 2011;1:1-8.
     Ashish Masih, Amar Kumar, Shivam Singh, Ajay Kumar Tiwari. Fast dissolving tablets: a review. Int J Curr Pharm Res 2017;9(2):8 18.
     IRC FDA - Drug Information - ODT
-    Puzzle out Machine Learning Model-Explaining Disintegration Process in ODTs - DOI : 10.3390/pharmaceutics14040859`.split("\n"));
+    Predicting oral disintegrating tablet formulations by neural network techniques - DOI : 10.1016/j.ajps.2018.01.003`.split("\n"));
 
     return (
         <div style={{'maxHeight':400,'overflow':'scroll'}}>
